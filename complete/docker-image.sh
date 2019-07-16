3a29a1e63bfbc55ad3e8028c2cce43ead21f8046
Eloi Mendes <eloi.mendes@omni.com.br>
2019-07-16 11:14:56 -0300

    gerar a imagem docker

---
+++ complete/docker-image.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t eloi-docker-multi/gs-spring-boot:0.1.1 .
--- complete/mvn-docker.sh
+++ complete/mvn-docker.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 docker run -v ~/.m2:/root/.m2 -v "$PWD":/usr/src -w /usr/src maven:3.5.2-jdk-8-alpine mvn package
+docker run -v ~/.m2:/root/.m2 -v "$PWD":/usr/src -w /usr/src maven:3.5.2-jdk-8-alpine mvn help:evaluate -Dexpression=project.version | grep -o "^[0-9]*\.[0-9]*\.[0-9]*" > target/release
